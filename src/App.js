be9bf2b5d440c867a184f43d2178d7b553a36c25
AnkitKachhadiya <28569675+AnkitKachhadiya@users.noreply.github.com>
2022-03-29 19:35:12 -0400

    Axios pet list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { ThemeProvider } from "styled-components";
 import NavBar from "./components/NavBar";
+import GlobalStyles from "./components/styles/Global";
+
+const theme = {
+    colors: {
+        header: "#003333",
+        body: "#fff",
+        footer: "#003333",
+    },
+    mobile: "768px",
+};
 
 function App() {
-    return <NavBar />;
+    return (
+        <ThemeProvider theme={theme}>
+            <GlobalStyles />
+            <NavBar />
+        </ThemeProvider>
+    );
 }
 
 export default App;
--- src/components/Pets.js
+++ src/components/Pets.js
@@ -1,7 +1,42 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const API_URL = "http://eulerity-hackathon.appspot.com/pets";
+
 function Pets() {
+    const [petList, setPetList] = useState();
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const { data } = await axios.get(API_URL);
+                setPetList(data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        fetchData();
+    }, []);
+
     return (
         <>
             <h1>Pets</h1>
+
+            {petList &&
+                petList.length > 0 &&
+                petList.map((currentPet, currentIndex) => (
+                    <div key={currentIndex}>
+                        <img
+                            src={currentPet.url}
+                            alt={currentPet.title}
+                            width="150"
+                            height="150"
+                        />
+                        <p>{currentPet.title}</p>
+                        <p>{currentPet.description}</p>
+                    </div>
+                ))}
         </>
     );
 }
